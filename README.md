5f09ba44304919accd6f024d4929f3590b0b5f84
Abnersilvacpu <abnerluis123@outlook.com>
2021-06-20 15:29:56 -0300

    inicio do projeto

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Atividade-PHP-e-PDO
+# projeto_web_2021
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+require("bd/conexao.php");
+
+?>
+
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Site</title>
+
+        <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <header>
+                <h1>Meu site</h1>
+            </header>
+
+            <div class="menu">
+                <ul>
+                    <a href="?pg=inicio"><li>Início</li></a>
+                    <a href="?pg=sobre"><li>Sobre</li></a>
+                    <a href="?pg=contato/formulario"><li>Contato</li></a>
+                    <?php 
+                        if(!isset($_SESSION["nome"])){
+                    ?>
+                        <a href="?pg=login/formulario"><li>Login</li></a>
+                    <?php
+                        }
+                        else{
+                    ?>
+                        <a href="?pg=area_restrita"><li>Área restrita</li></a>
+                    <?php
+                        }
+                    ?>
+                </ul>
+            </div>
+
+            <main>
+            
+                <?php
+
+                    /* Operador ternário para verificar se o pg está setado no GET e não está vazio
+                        Caso verdadeiro: usa o valor do GET["pg"]
+                        Caso falso: usa o valor "inicio"
+                    */
+                    $pg = (isset($_GET["pg"]) && !empty($_GET["pg"])) ? $_GET["pg"] : "inicio";
+
+                    include("paginas/".$pg.".php");
+
+                ?>
+
+            </main>
+
+            <footer>
+                <h4>Copyright &copy; 2021</h4>
+            </footer>
+
+        </div>
+
+    </body>
+    
+</html>
